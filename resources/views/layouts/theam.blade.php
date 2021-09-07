70e1acd8c6104fe555d79a9ee2eefdd7194e37f7
hima-chetriya <himachetriya@gmail.com>
2021-09-07 12:55:45 +0530

    added loader

--- resources/views/layouts/theam.blade.php
+++ resources/views/layouts/theam.blade.php
@@ -24,9 +24,9 @@
 <body class="dashboard-analytics">
 
     <!-- BEGIN LOADER -->
-    {{-- <div id="load_screen"> <div class="loader"> <div class="loader-content">
+    <div id="load_screen"> <div class="loader"> <div class="loader-content">
         <div class="spinner-grow align-self-center"></div>
-    </div></div></div> --}}
+    </div></div></div>
     <!--  END LOADER -->
 
     <!--  BEGIN NAVBAR  -->
