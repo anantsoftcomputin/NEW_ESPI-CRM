ab72e10a75e59f71c6a3baee575323c5b5a7c094
hima-chetriya <himachetriya@gmail.com>
2021-09-08 17:18:13 +0530

    agent refral

---
+++ app/Http/Controllers/ReferralCodeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReferralCode;
+use Illuminate\Http\Request;
+
+class ReferralCodeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ReferralCode  $referralCode
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReferralCode $referralCode)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ReferralCode  $referralCode
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ReferralCode $referralCode)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ReferralCode  $referralCode
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReferralCode $referralCode)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReferralCode  $referralCode
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReferralCode $referralCode)
+    {
+        //
+    }
+}
