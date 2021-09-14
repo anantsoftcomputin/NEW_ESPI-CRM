411e83f75d326ba8d2a90de4a7368c6335114bc6
hima-chetriya <himachetriya@gmail.com>
2021-09-14 13:09:28 +0530

    publish sir changes

--- resources/views/enquiry/detail_staps/stap_5.blade.php
+++ resources/views/enquiry/detail_staps/stap_5.blade.php
@@ -23,29 +23,33 @@
     </div>
     <div class="col-md-6">
     </div>
-
-    <div class="col-md-6 refusal_hide">
+</div>
+<div class="row">
+    <div class="col-md-6">
         <div class="form-group">
             <label for="name">Refusal Country</label>
             <input type="text" name="refusal_country" class="form-control">
         </div>
     </div>
-    <div class="col-md-6 refusal_hide">
+    <div class="col-md-6">
         <div class="form-group">
             <label for="name">Visa Catagory</label>
             <input type="text" name="refusal_country" class="form-control">
         </div>
     </div>
-    <div class="col-md-6 refusal_hide">
+    <div class="col-md-6">
         <div class="form-group">
             <label for="name">Refusal Resion</label>
             <input type="text" name="refusal_resion" class="form-control">
         </div>
     </div>
-    <div class="col-md-6 refusal_hide">
+    <div class="col-md-6">
         <div class="form-group">
             <label for="name">Refusal Date</label>
             <input type="date" name="refusal_date" class="form-control">
         </div>
     </div>
 </div>
+<a class="btn btn-info" style="float: right;" id="refusal_add">Add Refusal</a>
+<br>
+<br>
