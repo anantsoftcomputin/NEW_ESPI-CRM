0ad61cc50608697ff1d287d6d944848c6040f77c
sagarthakor <saggyt19@gmail.com>
2021-09-16 16:56:29 +0530

    theamfile-auth-make-null

--- resources/views/layouts/theam.blade.php
+++ resources/views/layouts/theam.blade.php
@@ -178,7 +178,7 @@
                             <div class="media mx-auto">
                                 <img src="{{ asset('assets/img/90x90.jpg') }}" class="img-fluid mr-2" alt="avatar">
                                 <div class="media-body">
-                                    <h5>{{ Auth::user()->name }}</h5>
+                                    <h5>{{ Auth::user()->name ?? "Admin" }}</h5>
                                     <p>Admin</p>
                                 </div>
                             </div>
