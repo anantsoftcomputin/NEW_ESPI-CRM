9f29594856cd3fd2db3d2184100de5abe4e28fb4
jasmin-shukla <mr.jasmin.shukal@gmail.com>
2021-11-28 10:56:25 +0530

    Detail ui step 01 -- massages

--- resources/views/enquiry/detail_ui/update.blade.php
+++ resources/views/enquiry/detail_ui/update.blade.php
@@ -1,4 +1,9 @@
 <div class="p-2">
-    <h1>Update</h1>
+    <div class="jumbotron">
+        <h1 class="display-4">Hello, {{ \Auth::user()->name }}!</h1>
+        <p class="lead">Sorry For The Inconvenience.</p>
+        <hr class="my-4">
+        <p>This functionality under Devlopment.</p>
+    </div>
 </div>
 
