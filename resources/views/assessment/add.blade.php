a02a93146eb45e7c1b2906035a0ad146276e2208
jasmin-shukla <mr.jasmin.shukal@gmail.com>
2021-12-01 15:37:24 +0530

    Ass condisnal changes 02

--- resources/views/assessment/add.blade.php
+++ resources/views/assessment/add.blade.php
@@ -77,7 +77,7 @@ Add Assessment
                                     <td>{{ $item->created_at->diffForHumans() }} By <span class="badge badge-warning"> {{ $item->AddedBy->name }}</span></td>
                                     <td class="text-center">
                                         @if ($item->status != "process")
-                                            Applyed
+                                            Applied
                                         @else
                                             <a href="{{ route('Assessment.Apply',$item->id) }}" title="Apply"><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle t-icon t-hover-icon"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg></a>
                                             <a href="{{ route('Assessment.Remove',$item->id) }}"><svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x t-icon t-hover-icon"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></a>
