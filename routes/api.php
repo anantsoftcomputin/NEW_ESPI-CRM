375bd546ad7e599581bb9de54e7b7c71276ca111
anantsoftcomputin <anantsoftcomputing@gmail.com>
2021-12-21 10:45:24 +0530

    Add option

--- routes/api.php
+++ routes/api.php
@@ -27,23 +27,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    Route::prefix('admin')->group(function () {
-        Route::get("getDetailsFromCourse/{course?}",[CourseController::class, 'getCourseDetails']);
-        Route::get("getCourseFromLevel/{level?}/{university?}",[CourseController::class, 'getCourse']);
-        Route::get("getEnquiry/{email?}",[EnquireController::class,"getEnquiryByEmail"]);
-        Route::get("checkemail/{email?}",[EnquireController::class,"checkEmail"]);
-        Route::get("otp_send/{email?}",[EnquireController::class,'sendOtp']);
-        Route::get('inquiry/{id?}',[ApplicationController::class,'detailFromEnq']);
-        Route::get('inquiry/FollowUp/{id?}',[FollowUpController::class,'ListByEnquiry']);
-        Route::get('getUniversityFromCountry/{country_id}',[UniversityController::class,'getUniversityFromCountry']);
-        Route::get('getCourseFromUniversity/{university_id}',[CourseController::class,'getCourseFromUniversity']);
-        Route::get('university_campus/delete/{id}',[UniversityController::class,'university_campus_delete']);
-        Route::get('getReferralByCode/{code}/',[ReferralCodeController::class,'getReferralByCode']);
-        Route::get('getStateById/{state_id}/',[StateController::class,'getStateById']);
-        Route::get('getCityById/{state_id}/',[CityController::class,'getCityById']);
-        Route::get('getState/{country_id}',[StateController::class,'getStateByCountry']);
-        Route::get('getCity/{state_id}',[CityController::class,'getCityByState']);
-        Route::get("assign_user/{user_id?}/{assessment_id?}",[AssessmentController::class,'assessmentAssign']);
-    });
+
+Route::prefix('admin')->group(function () {
+    Route::get("getDetailsFromCourse/{course?}",[CourseController::class, 'getCourseDetails']);
+    Route::get("getCourseFromLevel/{level?}/{university?}",[CourseController::class, 'getCourse']);
+    Route::get("getEnquiry/{email?}",[EnquireController::class,"getEnquiryByEmail"]);
+    Route::get("checkemail/{email?}",[EnquireController::class,"checkEmail"]);
+    Route::get("otp_send/{email?}",[EnquireController::class,'sendOtp']);
+    Route::get('inquiry/{id?}',[ApplicationController::class,'detailFromEnq']);
+    Route::get('inquiry/FollowUp/{id?}',[FollowUpController::class,'ListByEnquiry']);
+    Route::get('getUniversityFromCountry/{country_id}',[UniversityController::class,'getUniversityFromCountry']);
+    Route::get('getCourseFromUniversity/{university_id}',[CourseController::class,'getCourseFromUniversity']);
+    Route::get('university_campus/delete/{id}',[UniversityController::class,'university_campus_delete']);
+    Route::get('getReferralByCode/{code}/',[ReferralCodeController::class,'getReferralByCode']);
+    Route::get('getStateById/{state_id}/',[StateController::class,'getStateById']);
+    Route::get('getCityById/{state_id}/',[CityController::class,'getCityById']);
+    Route::get('getState/{country_id}',[StateController::class,'getStateByCountry']);
+    Route::get('getCity/{state_id}',[CityController::class,'getCityByState']);
+    Route::get("assign_user/{user_id?}/{assessment_id?}",[AssessmentController::class,'assessmentAssign']);
 });
