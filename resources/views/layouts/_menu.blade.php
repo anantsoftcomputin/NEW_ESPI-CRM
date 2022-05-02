4867f582484697d781753f65f828388f1c04cf21
jess <anantsoftcomputing@gmail.com>
2022-05-02 13:28:20 +0530

    test

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -11,6 +11,9 @@
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
 </li> --}}
 
+
+
+
 @can(["view-enquiry"])
 <li class="menu @if(\Request::route()->getName()=='Enquires.index') active @endif menu-single">
     <a href="{{ route('Enquires.index') }}" @if(\Request::route()->getName()=='Enquires.index') data-active="true" @else data-active="false" @endif class="menu-toggle">
@@ -157,7 +160,7 @@
 @endcan --}}
 
 
-@canany(['view-user','view-role','view-university','view-course'])
+@canany(['view-user','view-role','view-university','view-course','view-card'])
 <li class="menu">
     <a href="#setting" data-active="false" class="menu-toggle">
         <div class="base-menu">
