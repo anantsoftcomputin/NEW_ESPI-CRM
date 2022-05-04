b133d0be53255d3b5f83982ef580417ac1a688a5
jess <anantsoftcomputing@gmail.com>
2022-05-04 17:25:00 +0530

    Update _menu.blade.php

--- resources/views/layouts/_menu.blade.php
+++ resources/views/layouts/_menu.blade.php
@@ -188,29 +188,44 @@
 </li>
 @endcan
 
-{{-- <li class="menu @if(\Request::route()->getName()=='asset.index') active @endif menu-single">
-    <a href="{{ route('Asset.index') }}" @if(\Request::route()->getName()=='asset.index') data-active="true" @else data-active="false" @endif class="menu-toggle">
+{{-- <li class="menu  menu-single">
+    <a href="{{ route('/package') }}"  class="menu-toggle">
         <div class="base-menu">
             <div class="base-icons">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
             </div>
-            <span>Asset</span>
+            <span>package</span>
         </div>
     </a>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
 </li> --}}
 
 
-{{-- @can(["view-application_status"])
+@can(["view-application_status"])
 <li class="menu menu-single">
-    <a href="{{ url('/admin/ApplicationStatus') }}" data-active="false" class="menu-toggle">
+    <a href="{{ url('/admin/card') }}" data-active="false" class="menu-toggle">
         <div class="base-menu">
             <div class="base-icons">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path><line x1="7" y1="7" x2="7.01" y2="7"></line></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-grid"><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
             </div>
-            <span>Status</span>
+            <span>card</span>
         </div>
     </a>
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
 </li>
-@endcan --}}
+@endcan
+
+@can(["view-application_status"])
+<li class="menu menu-single">
+    <a href="{{ url('/admin/package') }}" data-active="false" class="menu-toggle">
+        <div class="base-menu">
+            <div class="base-icons">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tag"><path d="M20.59 13.41l-7.17 7.17a2 2 0 0 1-2.83 0L2 12V2h10l8.59 8.59a2 2 0 0 1 0 2.82z"></path><line x1="7" y1="7" x2="7.01" y2="7"></line></svg>
+            </div>
+            <span>package</span>
+        </div>
+    </a>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg>
+</li>
+@endcan
+
