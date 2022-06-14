3499837196948cf16558c5afe52e391a1221bbca
Ankur98563 <107184257+Ankur98563@users.noreply.github.com>
2022-06-14 15:44:13 +0530

    Update index.blade.php

--- resources/views/assessment/index.blade.php
+++ resources/views/assessment/index.blade.php
@@ -14,6 +14,10 @@
 <script src="https://cdn.jsdelivr.net/npm/handlebars@latest/dist/handlebars.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.7.7/handlebars.min.js" integrity="sha512-RNLkV3d+aLtfcpEyFG8jRbnWHxUqVZozacROI4J2F1sTaDqo1dPQYs01OMi1t1w9Y2FdbSCDSQ2ZVdAC8bzgAg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
+<script src="js/jquery.dataTables.js" type="text/javascript"></script>
+
+<script src="js/jquery.js" type="text/javascript"></script>
+
 <script type="text/javascript">
 
     $(function () {
